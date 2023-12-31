b7568dfb16a5c425c46d103d8a4f31be1b876260
imooje <savvyplus020@gmail.com>
2023-12-31 11:41:53 +0100

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import Home from "./pages/home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/savvy-app/",
+    element: <App />,
+    children: [
+      {
+        path: "/savvy-app/",
+        element: <Home />,
+      },
+      {
+        path: "/savvy-app/login",
+        element: <Login />,
+      },
+      {
+        path: "/savvy-app/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
