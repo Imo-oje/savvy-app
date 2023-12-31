831fe49feac87b025165c14ff44e7d35481429e1
imooje <savvyplus020@gmail.com>
2023-12-31 11:52:58 +0100

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +18,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-
+      <nav>
+        <Link to="/savvy-app/">Home</Link>
+        {" | "}
+        <Link to="/savvy-app/login">Login</Link>
+        {" | "}
+        <Link to="/savvy-app/register">Register</Link>
+      </nav>
       <Outlet />
 
       <p className="read-the-docs">
